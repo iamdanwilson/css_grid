75b717d204812037406821699bf6aaa9d5518803
iamdanwilson@hotmail.co.uk <iamdanwilson@hotmail.co.uk>
2021-02-20 09:11:14 +0000

    First commit for template project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import DufcCrest from './images/dufc-crest.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Hello World!</p>
+                <img src={DufcCrest} alt="dufc crest"/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
