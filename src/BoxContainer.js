ba15710588eff30c48d6607db6444ebbd37cd841
iamdanwilson <55436443+iamdanwilson@users.noreply.github.com>
2021-02-20 11:45:05 +0000

    Created header with pics on either side. havent managed to centre text yet

---
+++ src/BoxContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BoxContainer() {
+    return (
+        <div id="box-container">
+            <div id="box-1"></div>
+            <div id="box-2"></div>
+        </div>
+    )
+}
+export default BoxContainer;
