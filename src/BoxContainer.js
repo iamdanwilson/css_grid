a8c9ac87b911e1b3ffb1b5be241dad20218d911f
iamdanwilson <55436443+iamdanwilson@users.noreply.github.com>
2021-02-20 12:18:28 +0000

    Centred header text and added topic column on left

--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Topics from "./Topics";
 
 function BoxContainer() {
     return (
         <div id="box-container">
-            <div id="box-1"></div>
-            <div id="box-2"></div>
+            <div id="box-1">
+                <Topics />
+            </div>
+            <div id="box-2"/>
         </div>
     )
 }
