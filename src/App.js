77920ceee3a0abf92480eaf9da761cb5f5e4e546
iamdanwilson <55436443+iamdanwilson@users.noreply.github.com>
2021-02-26 07:56:12 +0000

    Adding a scrolling image bar and made all content responsive to viewport becoming very narrow

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './styles/App.css';
 import PicHeader from "./PicHeader";
 import MixesFlexBox from "./MixesFlexBox";
+import ScrollingBanner from "./ScrollingBanner";
 
 function App() {
     return (
         <div className="App-Container">
             <PicHeader />
                 <div className="topics-panel">Topics</div>
+                <div className="scrolling-panel">
+                    <ScrollingBanner />
+                </div>
                 <div className="main-panel">
-                    Mixes
                     <MixesFlexBox />
                 </div>
                 <div className="ads-panel">Adverts</div>
